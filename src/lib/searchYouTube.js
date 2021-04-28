877fba4618a10e1ef7c7576e9dd77f7ce512621b
sophiacheong <sophiaacheong@gmail.com>
2021-04-28 16:56:27 -0700

    added GET jquery

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,18 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: options.key,
+      q: options.query,
+      part: 'snippet',
+      maxResults: options.max,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    success: function(data) { callback(data.items); },
+    error: (response) => { throw response; }
+  });
 };
 
 export default searchYouTube;
