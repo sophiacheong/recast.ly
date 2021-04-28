091301e216beabf728640a0d098762b009b22fa7
sophiacheong <sophiaacheong@gmail.com>
2021-04-28 16:30:43 -0700

    added onclick

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,48 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      current: exampleVideoData[0],
+    };
+    this.onClickTitle = this.onClickTitle.bind(this);
+  }
+
+  onClickTitle(e) {
+    this.state.videos.map((item) => {
+      if (item.snippet.title === e.target.textContent) {
+        this.setState({
+          current: item,
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em><Search /></em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em><VideoPlayer video={this.state.current} /></em> view goes here</h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em><VideoList videos={this.state.videos} onClickTitle={this.onClickTitle} /></em> view goes here</h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,11 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos, onClickTitle}) => (
+  videos.map((item, index) => (
+    <div className="video-list">
+      <div><h5><em><VideoListEntry clickTitle={onClickTitle} video={item} key={item.etag} /></em> view goes here </h5></div>
+    </div>
+  ))
 );
 
 // PropTypes tell other developers what `props` a component expects
